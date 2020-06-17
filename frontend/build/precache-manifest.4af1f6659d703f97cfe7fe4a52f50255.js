df748008c290bf2a44da299e74c06eef9b1ff2d6
Ray Wang <7299493+pivstone@users.noreply.github.com>
2020-06-18 00:26:30 +0200

    feat: add build

---
+++ frontend/build/precache-manifest.4af1f6659d703f97cfe7fe4a52f50255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4bc25956a6d5e883cd56cb24c8a11e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cfa09439e0a25772a5a",
+    "url": "/static/css/main.1675ada8.chunk.css"
+  },
+  {
+    "revision": "3bebee025f817a6df7a9",
+    "url": "/static/js/2.85271563.chunk.js"
+  },
+  {
+    "revision": "70291cd18ec1546f130cb1ac96daa386",
+    "url": "/static/js/2.85271563.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cfa09439e0a25772a5a",
+    "url": "/static/js/main.9779b02f.chunk.js"
+  },
+  {
+    "revision": "4bb50a056bff6b6ad2a5",
+    "url": "/static/js/runtime-main.05d23dce.js"
+  }
+]);
